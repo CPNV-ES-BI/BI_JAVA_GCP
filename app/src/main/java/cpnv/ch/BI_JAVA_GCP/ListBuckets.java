a0da4518d78b1deac485c4b463459b90b6a90266
robiel <robiel.tesfazghi@cpnv.ch>
2022-12-07 12:26:54 +0100

    Display listBuckets

--- app/src/main/java/cpnv/ch/BI_JAVA_GCP/ListBuckets.java
+++ app/src/main/java/cpnv/ch/BI_JAVA_GCP/ListBuckets.java
@@ -5,16 +5,21 @@ import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
+import java.util.LinkedList;
+
 public class ListBuckets {
-    public static void listBuckets(String projectId) {
+    public static LinkedList listBuckets(String projectId) {
         // The ID of your GCP project
         // String projectId = "your-project-id";
 
         Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
         Page<Bucket> buckets = storage.list();
+        LinkedList listBuckes = null;
 
         for (Bucket bucket : buckets.iterateAll()) {
+            listBuckes.add(bucket.getName());
             System.out.println(bucket.getName());
         }
+        return listBuckes;
     }
 }
--- app/src/main/java/cpnv/ch/BI_JAVA_GCP/Main.java
+++ app/src/main/java/cpnv/ch/BI_JAVA_GCP/Main.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+import java.util.Properties;
+
 @SpringBootApplication
 @RestController
 public class Main {
-
+    private static ListBuckets buckets;
     @RequestMapping("/")
     public String home() {
         return "Hello Docker World";
@@ -16,5 +19,12 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+        Properties prop = new Properties();
+        String propFileName = "config.properties";
+        String projectId = prop.getProperty("projectId");
+
+        LinkedList listBuckes = buckets.listBuckets(projectId);
+
     }
 }
