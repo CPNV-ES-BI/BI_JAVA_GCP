33eacf6e7cbbf2920019523fcbbee70cbb13f352
Nomeos <mathieu.rabot01@gmail.com>
2022-12-16 14:27:40 +0100

    Create the project with spring initializr

---
+++ BI-Project/src/main/java/com/cpnv/BIJAVAGCP/BiJavaGcpApplication.java
@@ -0,0 +1,13 @@
+package com.cpnv.BIJAVAGCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiJavaGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiJavaGcpApplication.class, args);
+	}
+
+}
---
+++ BI-Project/src/test/java/com/cpnv/BIJAVAGCP/BiJavaGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpnv.BIJAVAGCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiJavaGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
