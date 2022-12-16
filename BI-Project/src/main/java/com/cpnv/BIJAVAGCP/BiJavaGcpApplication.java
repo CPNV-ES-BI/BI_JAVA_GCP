3e7768ac6474ab5b0991c8896c0dec2b1322b012
Nomeos <mathieu.rabot01@gmail.com>
2022-12-16 15:03:32 +0100

    Print Hello World on the page (without docker)

--- BI-Project/src/main/java/com/cpnv/BIJAVAGCP/BiJavaGcpApplication.java
+++ BI-Project/src/main/java/com/cpnv/BIJAVAGCP/BiJavaGcpApplication.java
@@ -2,12 +2,19 @@ package com.cpnv.BIJAVAGCP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BiJavaGcpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BiJavaGcpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BiJavaGcpApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
 }
