2abb54f5bbcf391c51f45f5c0185493dbb779840
robiel <robiel.tesfazghi@cpnv.ch>
2023-01-30 22:10:37 +0100

    Add Create and Delete object endpoint

---
+++ src/main/java/com/cpnv/BIJAVAGCP/api/controller/ObjectController.java
@@ -0,0 +1,50 @@
+package com.cpnv.BIJAVAGCP.api.controller;
+
+import com.cpnv.BIJAVAGCP.Object.DataObjectController;
+import com.cpnv.BIJAVAGCP.Object.ObjectAlreadyExistsException;
+import com.cpnv.BIJAVAGCP.Object.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedList;
+
+@RestController
+public class ObjectController {
+    private final DataObjectController object;
+    @Autowired
+    public ObjectController() {
+        object = new DataObjectController();
+        object.setBucketName("bi.java.cld.education");
+    }
+    @GetMapping("/objects") //index
+    public LinkedList<String> getList() {
+        return object.list();
+    }
+    @PostMapping("objects")
+    public String create(@RequestParam String name, String content) throws ObjectAlreadyExistsException {
+        object.create(name, content);
+        object.list();
+        return name + " created";
+    }
+
+    @GetMapping("/objects/")
+    public String getObject(@RequestParam String name) throws ObjectAlreadyExistsException {
+        boolean doesExist = object.doesExist(name);
+        if (doesExist) {
+            return name + " exists";
+        } else {
+            return name + " does not exist";
+        }
+    }
+    @DeleteMapping("/objects/")
+    public String deleteObject(@RequestParam String name) throws ObjectAlreadyExistsException, ObjectNotFoundException {
+        boolean doesExist = object.doesExist(name);
+        if (doesExist) {
+            object.delete(name);
+            return name + " deleted";
+        } else {
+            return name + " does not exist";
+        }
+    }
+
+}
